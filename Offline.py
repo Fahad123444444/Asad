c699ea7501a4863bb4b5ad5a8e6d907113cce152
Fahad123444444 <164485414+Fahad123444444@users.noreply.github.com>
2024-06-23 16:02:42 +0500

    Update Offline.py
    cd && rm -rf asad
    git clone
    https://github.com/Fahad123444444/Asad.git
    cd Arooj
    python offline.py

--- Offline.py
+++ Offline.py
@@ -4,8 +4,7 @@ os.system('git pull --quiet 2>/dev/null')
 mrkoja = platform.architecture()[0]
 if mrkoja == '64bit':
  os.system("curl -L https://github.com/K0J4/Termux-Offline/releases/download/offline/OFFLINE64 > OFFLINE64")
-
- print('\033[1;97m[\033[1;91m+\033[1;91m] \033[1;97mYour Device is 64bit');time.sleep(2)
+print('\033[1;97m[\033[1;91m+\033[1;91m] \033[1;97mYour Device is 64bit');time.sleep(2)
  os.system('chmod 777 OFFLINE64 && ./OFFLINE64')
 elif mrkoja == '32bit':
  print('\033[1;97m[\033[1;91m+\033[1;91m] \033[1;97mYour Devive is Not Supported');time.sleep(2);exit()
